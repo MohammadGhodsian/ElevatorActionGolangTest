e9335819f83bc8cabd6cec7595355c66cd9797fd
Misagh <mohammad.ghodsian@gmail.com>
2024-07-31 02:38:17 +0330

    main test func

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Direction bool
 
 const (
@@ -30,4 +32,22 @@ func (e *Elevator) move() {
 	} else if e.Direction == Down {
 		e.CurrentFloor--
 	}
+}
+
+func Order(startingFloor int, queue []Person) []int {
+	return []int{}
+}
+
+func main() {
+	queue := []Person{
+		{From: 5, To: 4},  // 1st passenger
+		{From: 5, To: 3},  // 2nd passenger
+		{From: 3, To: 4},  // 3rd passenger
+		{From: 0, To: 2},  // 5th passenger
+		{From: 3, To: -4}, // 4th passenger
+		{From: 1, To: 2},  // 6th passenger
+	}
+	startingFloor := 5
+	result := Order(startingFloor, queue)
+	fmt.Println(result) // Expected: []int{5, 4, 3, 4, 3, -4, 0, 1, 2}
 }
