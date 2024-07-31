28c103268ff30b7cb53a8d37a5e7ae2c23369e8a
Misagh <mohammad.ghodsian@gmail.com>
2024-07-31 04:34:17 +0330

    last test passed  [5 4 3 4 3 -4 0 1 2]

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Direction bool
 
@@ -49,7 +51,7 @@ func Order(startingFloor int, queue []Person) []int {
 	// current floor of elevator in each step
 	currentFloor := startingFloor
 	// intial the direction of elevator by first person in list
-	var directionElev Direction
+	var directionElev Direction = direction(currentFloor, queue[0].From)
 	// // create queue of up and down (passengers directions)
 	// downQueue := []Person{}
 	// upQueue := []Person{}
@@ -69,19 +71,14 @@ func Order(startingFloor int, queue []Person) []int {
 	elevatorMustMove := func() bool {
 		return len(queue) > 0 || len(passengers) > 0
 	}
+
 	for elevatorMustMove() {
-		// Manage the direction based on the passenger queue and destination
-		if len(passengers) == 0 {
-			directionElev = direction(currentFloor, queue[0].From)
-		} else {
-			directionElev = direction(currentFloor, passengers[0].To)
-		}
 
 		// Drop off passengers
 		droppedOff := false
 		for i := 0; i < len(passengers); i++ {
 			if passengers[i].To == currentFloor {
-				fmt.Printf("droppedOff  %v   %v  currentFloor %d\n", droppedOff, passengers[i], currentFloor)
+				fmt.Printf("droppedOff  %v   %v  currentFloor %d     queue %+v   passengers %+v\n", droppedOff, passengers[i], currentFloor, queue, passengers)
 				passengers = append(passengers[:i], passengers[i+1:]...)
 				i--
 				droppedOff = true
@@ -91,11 +88,26 @@ func Order(startingFloor int, queue []Person) []int {
 			addStop(currentFloor)
 		}
 
+		if !elevatorMustMove() {
+			break
+		}
+
+		// // Manage the direction based on the passenger queue and destination
+		if len(passengers) == 0 {
+			if currentFloor != queue[0].From {
+				directionElev = direction(currentFloor, queue[0].From)
+			} else {
+				directionElev = direction(currentFloor, queue[0].To)
+			}
+		} else {
+			directionElev = direction(currentFloor, passengers[0].To)
+		}
+
 		// Pick up passengers
 		pickedUp := false
 		for i := 0; i < len(queue); i++ {
 			if queue[i].direction() == directionElev && queue[i].From == currentFloor {
-				fmt.Printf("pickedUp  %v   %v    currentFloor %d\n", pickedUp, queue[i], currentFloor)
+				fmt.Printf("pickedUp  %v   %v    currentFloor %d    queue %+v   passengers %+v\n", pickedUp, queue[i], currentFloor, queue, passengers)
 				passengers = append(passengers, queue[i])
 				queue = append(queue[:i], queue[i+1:]...)
 				i--
@@ -107,27 +119,35 @@ func Order(startingFloor int, queue []Person) []int {
 		}
 
 		// Determine the next floor to move to
-		// if len(passengers) > 0 {
-		if elevatorMustMove() && directionElev == Up {
-			currentFloor++
-		} else {
-			currentFloor--
+		if elevatorMustMove() {
+			if directionElev == Up {
+				currentFloor++
+			} else {
+				currentFloor--
+			}
 		}
-		// }
 	}
 
 	return resultStops
 }
 
 func main() {
-	queue := []Person{
-		{From: 3, To: 2}, // Al
-		{From: 5, To: 2}, // Betty
-		{From: 2, To: 1}, // Charles
-		{From: 2, To: 5}, // Dan
-		{From: 4, To: 3}, // Ed
-	}
-	startingFloor := 1
+	queue := []Person{{From: 5, To: 4}, // 1st passenger
+		{From: 5, To: 3},  // 2nd passenger
+		{From: 3, To: 4},  // 3rd passenger
+		{From: 0, To: 2},  // 5th passenger
+		{From: 3, To: -4}, // 4th passenger
+		{From: 1, To: 2}}
+	startingFloor := 5
+	// queue := []Person{
+	// 	{From: 3, To: 2}, // Al
+	// 	{From: 5, To: 2}, // Betty
+	// 	{From: 2, To: 1}, // Charles
+	// 	{From: 2, To: 5}, // Dan
+	// 	{From: 4, To: 3}, // Ed
+	// }
+	// startingFloor := 1
+
 	result := Order(startingFloor, queue)
 	fmt.Println(result) // Expected: []int{5, 4, 3, 4, 3, -4, 0, 1, 2}
 }
